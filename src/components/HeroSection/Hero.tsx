bec07dfeaa6adcff759ee3910af161a57a8d13e4
festus-sulumeti <sulumetifestus@outlook.com>
2025-12-18 16:58:22 +0300

    feat: hero section place

--- src/components/HeroSection/Hero.tsx
+++ src/components/HeroSection/Hero.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar"
+import { Link } from "react-router-dom"
 
 
 function Hero() {
@@ -22,6 +23,68 @@ function Hero() {
         </div>
 
       </section>
+
+     <section>
+     
+        <div className="py-12 grid grid-cols-1 sm:grid-cols-2 gap-8 lg:gap-12">
+        
+        <Link className="group flex flex-col focus:outline-hidden" to="#">
+            <div className="aspect-w-16 aspect-h-12 overflow-hidden bg-gray-100 rounded-2xl dark:bg-neutral-800">
+            <img className="group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out object-cover rounded-2xl" 
+            src="https://images.unsplash.com/photo-1511920170033-f8396924c348?q=80&w=800&auto=format&fit=crop"
+            alt="Blog Image" />
+            </div>
+
+            <div className="pt-4">
+            <h3 className="relative inline-block font-medium text-lg text-black before:absolute before:bottom-0.5 before:start-0 before:-z-1 before:w-full before:h-1 before:bg-lime-400 before:transition before:origin-left before:scale-x-0 group-hover:before:scale-x-100 dark:text-white">
+                Signature Espresso
+            </h3>
+            <p className="mt-1 text-gray-600 dark:text-neutral-400">
+                Bold, intense espresso crafted from premium Rift Valley beans.
+            </p>
+
+            <div className="mt-3 flex flex-wrap gap-2">
+                <span className="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                Espresso
+                </span>
+                <span className="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                Dark Roast
+                </span>
+                <span className="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                Bold Flavor
+                </span>
+            </div>
+            </div>
+        </Link>
+        
+        <Link className="group flex flex-col focus:outline-hidden" to="#">
+            <div className="aspect-w-16 aspect-h-12 overflow-hidden bg-gray-100 rounded-2xl dark:bg-neutral-800">
+            <img className="group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out object-cover rounded-2xl" 
+            src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=800&auto=format&fit=crop"
+            alt="Blog Image" />
+            </div>
+
+            <div className="pt-4">
+            <h3 className="relative inline-block font-medium text-lg text-black before:absolute before:bottom-0.5 before:start-0 before:-z-1 before:w-full before:h-1 before:bg-lime-400 before:transition before:origin-left before:scale-x-0 group-hover:before:scale-x-100 dark:text-white">
+                Hand Brewed Pour-Over
+            </h3>
+            <p className="mt-1 text-gray-600 dark:text-neutral-400">
+                Clean, aromatic flavors highlighting the bean’s natural character.
+            </p>
+
+            <div className="mt-3 flex flex-wrap gap-2">
+                <span className="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                Pour Over
+                </span>
+                <span className="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                Single Origin
+                </span>
+            </div>
+            </div>
+        </Link> 
+        </div>
+
+     </section>
     </>
   )
 }
